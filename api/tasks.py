b158a1fca1780aee55153bb26ad763fc01ed70a9
Dmytruto <dmytruto@gmail.com>
2019-02-24 17:05:21 +0200

    load parts of video stream

--- api/tasks.py
+++ api/tasks.py
@@ -7,6 +7,8 @@ import cv2
 import numpy as np
 
 
+cap = cv2.VideoCapture(0)
+
 
 @task(schedule=1)
 def send_request():
@@ -18,18 +20,21 @@ def send_request():
 #       requests.put('http://127.0.0.1:8000/idealpost/', data=serializer.data)
 
 
-@task(schedule=100)
+@task(schedule=20)
 def perform_video_stream():
    start = time.time()
-   cap = cv2.VideoCapture(0)
+   fourcc = cv2.VideoWriter_fourcc(*'XVID')
+   out = cv2.VideoWriter('video/' + str(start) + '.avi', fourcc, 20.0, (640, 480))
    while True:
       ret, frame = cap.read()
+      frame = cv2.flip(frame, 0)
+      out.write(frame)
       cv2.imshow("frame", frame)
       end = time.time()
-      if int(end - start) > 10 or cv2.waitKey(1) == 13 :
+      dif = int(end - start)
+      print(dif)
+      if dif > 10 or cv2.waitKey(1) == 13 :
          break
-
-   cap.release()
    cv2.destroyAllWindows()
 
 
